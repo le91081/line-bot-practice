a3abf5b74ccfedf698a51763bd394a449f48d098
Bert <le91081@gmail.com>
2018-06-23 16:32:24 +0800

    test

--- app.py
+++ app.py
@@ -286,11 +286,14 @@ def panx():
         content += '{}\n{}\n\n'.format(title, link)
     return content
 
+def member():
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("event.reply_token:", event.reply_token)
     print("event.message.text:", event.message.text)
+    print("event",event)
     if event.message.text == "eyny":
         content = eyny_movie()
         line_bot_api.reply_message(
@@ -309,28 +312,6 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=content))
         return 0
-    # if event.message.text == "來張 imgur 正妹圖片":
-    #     client = ImgurClient(client_id, client_secret)
-    #     images = client.get_album_images(album_id)
-    #     index = random.randint(0, len(images) - 1)
-    #     url = images[index].link
-    #     image_message = ImageSendMessage(
-    #         original_content_url=url,
-    #         preview_image_url=url
-    #     )
-    #     line_bot_api.reply_message(
-    #         event.reply_token, image_message)
-    #     return 0
-    # if event.message.text == "隨便來張正妹圖片":
-    #     image = requests.get(API_Get_Image)
-    #     url = image.json().get('Url')
-    #     image_message = ImageSendMessage(
-    #         original_content_url=url,
-    #         preview_image_url=url
-    #     )
-    #     line_bot_api.reply_message(
-    #         event.reply_token, image_message)
-    #     return 0
     if event.message.text == "近期熱門廢文":
         content = ptt_hot()
         line_bot_api.reply_message(
@@ -527,26 +508,6 @@ def handle_message(event):
     #     )
     # )
     # line_bot_api.reply_message(event.reply_token, buttons_template)
-
-
-# # @handler.add(MessageEvent, message=StickerMessage)
-# # def handle_sticker_message(event):
-#     print("package_id:", event.message.package_id)
-#     print("sticker_id:", event.message.sticker_id)
-#     # ref. https://developers.line.me/media/messaging-api/sticker_list.pdf
-#     sticker_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 100, 101, 102, 103, 104, 105, 106,
-#                    107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125,
-#                    126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 401, 402]
-#     index_id = random.randint(0, len(sticker_ids) - 1)
-#     sticker_id = str(sticker_ids[index_id])
-#     print(index_id)
-#     sticker_message = StickerSendMessage(
-#         package_id='1',
-#         sticker_id=sticker_id
-#     )
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         sticker_message)
 
 
 if __name__ == '__main__':
