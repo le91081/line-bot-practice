aefc58253f28947b501420dc7c8aa08c0a094032
le91081 <le91081@gmail.com>
2019-01-29 01:11:16 +0800

    update

--- app.py
+++ app.py
@@ -797,13 +797,6 @@ def handle_message(event):
 
         print("----------------Start Search------------------------------")
         keyword = event.message.text.split('的')[1]
-
-        if isinstance(event.source,SourceRoom):
-            pass
-        elif isinstance(event.source,SourceUser):
-            user_id = event.source.user_id
-
-        line_bot_api.push_message(user_id,TextSendMessage(text="為您搜尋中..."))
         data = getNear(keyword)
         colAry = []
         if data == -1:
