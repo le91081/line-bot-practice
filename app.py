e69f9d57d5d6ca639120094f1b6f7ff759807d80
le91081 <le91081@gmail.com>
2019-01-29 01:24:43 +0800

    update

--- app.py
+++ app.py
@@ -808,9 +808,9 @@ def handle_message(event):
             return
 
         if len(data) > 0:
-            print('------Processing Data')
             colAry = []
-            datalen = max(10,len(data))
+            datalen = min(10,len(data))
+            print('------Processing Data Start')
             for i in range(datalen):
                 if data[i]['phtoUrl'] == '':
                     data[i]['phtoUrl'] = 'https://image.flaticon.com/icons/png/512/66/66455.png'
@@ -834,6 +834,7 @@ def handle_message(event):
                     ]
                 )
                 colAry.append(c)
+            print('------Processing Data End')
             #print(colAry)
             Carousel_template = TemplateSendMessage(
                 alt_text='Carousel template',
