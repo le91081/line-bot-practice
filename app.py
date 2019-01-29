4ba13049908a2cae4e006b85d56aca9fe74208d1
le91081 <le91081@gmail.com>
2019-01-29 15:44:11 +0800

    update

--- app.py
+++ app.py
@@ -649,6 +649,7 @@ def handle_message(event):
         return 0
 
     if event.message.text == '線上辦卡':
+        text_template = TextSendMessage(text='ｅ指辦卡，提供您線上快速申辦信用卡服務，申辦方式：\n(1) 本行存戶或正卡卡友：透過簡訊密碼驗證，線上快速辦卡。\n(2) 非存戶或新卡友：可以透過自然人憑證+讀卡機 或 指定銀行信用卡，檢附身分證影本、近三個月財力證明，免書面填寫、免郵寄，快速又便利。\n※ 僅限申辦正卡，若需申辦附卡可轉接專人索取申請書。')
         buttons_template = TemplateSendMessage(
             alt_text='線上辦卡 template',
             template=ButtonsTemplate(
@@ -670,7 +671,7 @@ def handle_message(event):
                 ]
             )
         )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
+        line_bot_api.reply_message(event.reply_token, [text_template,buttons_template])
         return 0
 
     if event.message.text == '申請信用卡的財力證明有哪些?':
@@ -979,27 +980,6 @@ def linePost(title, money, content, userid, roomid):
     db.session.add(p)
     db.session.commit()
     return True
-
-
-# @app.route("/post", methods=['POST'])
-# def postMethod():
-#     data = request.form
-#     print(data)
-#     p = post(title=data['title'], content=data['content'], money=data['money'])
-#     db.session.add(p)
-#     db.session.commit()
-#     return json.dumps({"status": 200, "comment": "Add Success"})
-
-
-# @app.route("/postByJson", methods=['POST'])
-# def postByJson():
-#     data = request.get_json()
-#     p = post(title=data['title'], content=data['content'],
-#              money=int(data['money']))
-#     db.session.add(p)
-#     db.session.commit()
-#     return json.dumps({"status": 200, "comment": "Add Success"})
-
 
 @app.route("/index")
 def index():
